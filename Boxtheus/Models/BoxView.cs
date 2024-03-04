83082ecd14eae80e3eb954d4f7a7263b6a9cb115
Eshika Rajubhai Lad <W0849742@myscc.ca>
2024-03-04 18:39:38 -0500

    Added validation rules to the model to ensure that the validation rules are enforced.

--- Boxtheus/Models/BoxView.cs
+++ Boxtheus/Models/BoxView.cs
@@ -31,7 +31,7 @@ namespace Boxtheus.Models
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(15)]
+        [StringLength(18)]
         [Required]
         public required String Color { get; set; }
     }
